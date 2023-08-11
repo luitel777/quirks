f5c6911ce22de111b7d5306ff5c67c2d4fa083dc
Aagaman Luitel <mail@luitelaagaman.com.np>
2023-08-11 17:46:27 +0545

    dynamic loading of library is cool

---
+++ dynamic/main.c
@@ -0,0 +1,29 @@
+#include <dlfcn.h>
+#include <stdio.h>
+
+#ifdef __ONE__
+#define LIBPRINT "libprint_1"
+#else
+#define LIBPRINT "libprint_2"
+#endif
+
+void (*something)();
+
+int main(){
+        char *error;
+        void *handle = dlopen(LIBPRINT, RTLD_LAZY);
+
+        if(!handle){
+                printf("error on dlopen %s\n", dlerror());
+                return 0;
+        }
+        something = dlsym(handle, "print_something");
+        if((error = dlerror()) != NULL){
+                printf("%s\n", error);
+        }
+        something();
+
+        dlclose(handle);
+
+        return 0;
+}
---
+++ dynamic/print_something_2.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void print_something(){
+        printf("two\n");
+}
